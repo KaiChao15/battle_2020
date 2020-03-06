91ab60514993c7f1e3c256d46b2014e5dc915a3f
Yu-Kai Chao <Kaichao@uvic.ca>
2020-03-06 00:27:13 -0800

    checking neck fixed

--- app/algorithm.py
+++ app/algorithm.py
@@ -205,11 +205,13 @@ class Algorithm:
         # True if calculating alpha at this depth, false if calculating beta
         if maxPlayer:
             moves = my_moves
-            moves.remove(state['me']['body'][1])
+            if state['me']['body'][1] in moves:
+                moves.remove(state['me']['body'][1])
             print("my moves:", moves)
         else:
             moves = enemy_moves
-            moves.remove(state['me']['body'][1])
+            if state['target']['body'][1] in moves:
+                moves.remove(state['me']['body'][1])
             print("enemy moves:", moves)
         # print("me: ", state['me']['body'][1]['x'])
         # print("target: ", state['target']['body'][1]['x'])
